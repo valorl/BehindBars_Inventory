79c57e2cade5328c606fb290686f0b8e119c23c4
Grivier1 <mauro_cesar_monteiro@hotmail.com>
2015-05-12 21:02:11 +0200

    test change

--- src/DBLayer/DBProductTest.java
+++ src/DBLayer/DBProductTest.java
@@ -20,22 +20,28 @@ public class DBProductTest {
 		testProduct.setName("Absolut Vodka");
 		testProduct.setCost(100.00);
 		testProduct.setPrice(300.00);
-		//testProduct.addQuantLoc(quantLocs);
 		testProduct.setUnitVolume(70);
 		testProduct.setType("Alcohol");
 		testProduct.setPurchased(20);
 		
-		QuantLoc  quantLocs = new QuantLoc();
-		quantLocs.setLocation("bar1");
-		quantLocs.setQuantity(70);
-		testProduct.addQuantLoc(quantLocs);
+		QuantLoc testQuantLoc1 = new QuantLoc();
+		testQuantLoc1.setLocation("bar1");
+		testQuantLoc1.setQuantity(70);
+		testProduct.addQuantLoc(testQuantLoc1);
+		
+		QuantLoc testQuantLoc2 = new QuantLoc();
+		testQuantLoc2.setLocation("bar2");
+		testQuantLoc2.setQuantity(70);
+		testProduct.addQuantLoc(testQuantLoc2);
+		
+		QuantLoc testQuantLoc3 = new QuantLoc();
+		testQuantLoc3.setLocation("bar3");
+		testQuantLoc3.setQuantity(70);
+		testProduct.addQuantLoc(testQuantLoc3);
 		
 
 		//Alcohol testAlcohol = new Alcohol();
 		
-
-
-
 		DBProduct dbProduct = new DBProduct();
 		try {
 			dbProduct.insertProduct(testProduct);
