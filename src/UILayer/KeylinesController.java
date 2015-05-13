66a6d96e1d219bca26e11599b066229b36b53fe7
Anettt <anett.92@gmail.com>
2015-05-13 17:53:14 +0200

    UI keylines

---
+++ src/UILayer/KeylinesController.java
@@ -0,0 +1,35 @@
+package UILayer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+
+public class KeylinesController implements Initializable, ChangeablePane{
+
+	PaneChanger changer;
+	
+	@FXML
+	private HBox mainHbox;
+	
+	@FXML
+	private Button btn_save;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		mainHbox.getStylesheets().addAll(getClass().getResource("inventory.css").toExternalForm());
+		
+	}
+	
+	@Override
+	public void setPaneParent(PaneChanger parent) {
+		changer = parent;		
+	}
+	
+	
+}
