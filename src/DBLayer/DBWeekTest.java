e4bbee23a9ff7ed59b19f99ae765ee7b10c9c743
AlisaHH <hellehansen.md@gmail.com>
2015-05-15 14:07:19 +0200

    Week test fixes
    fixes in insert and find

--- src/DBLayer/DBWeekTest.java
+++ src/DBLayer/DBWeekTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ModelLayer.Product;
+import ModelLayer.ProductState;
 import ModelLayer.Week;
 import ModelLayer.Employee;
 
@@ -17,6 +19,17 @@ public class DBWeekTest {
 		testWeek.setMonth(3);
 		testWeek.setYear(2015);	
 		
+		Product testProduct = new Product();
+		testProduct.setId(7);
+		testProduct.setName("Bombay Gin");
+		testProduct.setCost(100.00);
+		testProduct.setPrice(200.00);
+		testProduct.setUnitVolume(70);
+		testProduct.setType("Alcohol");
+		testProduct.setPurchased(20);
+				
+		ProductState state = new ProductState(testProduct);
+		testWeek.addState(state);
 
 		Employee employee = new Employee();
 		testWeek.setEmployee(employee);
@@ -38,14 +51,10 @@ public class DBWeekTest {
 		@Test
 		public void findWeekTest()
 		{
-			Week findWeek = new Week();
-			findWeek.getNumber();
-			findWeek.getMonth();
-			findWeek.getYear();
-			
+		
 			DBWeek dbWeek = new DBWeek();
 			try {
-				dbWeek.findWeek(2, 3, 2015, retriveAssociation);
+				dbWeek.findWeek(2, 3, 2015, true);
 				System.out.print("Week found");
 			}
 			catch(Exception ex)
